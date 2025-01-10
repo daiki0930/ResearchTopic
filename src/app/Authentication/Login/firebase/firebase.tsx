d12a6d5d8aabb9d8f07141debdabe5e6ac93bab8
daiki kuroiwa <daikikuroiwa@daikinoMacBook-Air.local>
2025-01-11 01:00:10 +0900

    微調整

--- src/app/Authentication/Login/firebase/firebase.tsx
+++ src/app/Authentication/Login/firebase/firebase.tsx
@@ -1,15 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAdknZU-076ySmJvT1C3EBccjtm06v88j0",
-  authDomain: "next1-c5784.firebaseapp.com",
-  projectId: "next1-c5784",
-  storageBucket: "next1-c5784.appspot.com",
-  messagingSenderId: "661541000684",
-  appId: "1:661541000684:web:5076e8ad5794bab37e1cc9",
-  measurementId: "G-3CGF53S9SZ"
-};
+if (!process.env.FIREBASE_SERCRET) {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT_KEYがありません。');
+}
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_SERCRET)
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
